07d2ba55ba0749dc66cb7a8a4894c9e95da51e46
KevinLa1999 <72019912+KevinLa1999@users.noreply.github.com>
2020-12-10 22:46:49 +0000

    Added more code and updated speadsheet
    - Progressed on code to get predicted output on Global Sales.
    -Changed excel spreadsheet so the rank for each data now fits as a lot of data was removed.

--- Python Code/VideoGameRegressionCode.py
+++ Python Code/VideoGameRegressionCode.py
@@ -10,21 +10,64 @@ path = "C:/Users/Kevin/Documents/GitHub/IntroToAI"
 
 filename_read = os.path.join(path, "vgsales.csv")
 df = pd.read_csv(filename_read, na_values=['NA', '?'])
+pd.set_option('display.max_columns', None)
 
-## check for null data
-print(df.isnull().any())# Strip non-numeric features from the dataframe
-df = df.select_dtypes(include=['int', 'char'])
+print(df[:400])
+
+print(df.isnull().any())
+
+# Strip non-numeric features from the dataframe
+df = df.select_dtypes(include=['int', 'float'])
+
+#print to check that this has worked
+print(df[:5]) 
+
 #collect the columns names for non-target features
 result = []
 for x in df.columns:
-    if x != 'NA_Sales':
+    if x != 'Global_Sales':
         result.append(x)
    
 X = df[result].values
-y = df['NA_Sales'].values
+y = df['Global_Sales'].values
 
+#split data into testing and training
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=320, random_state=0)
 
-#print to check that this has worked
-print(df[:400]) 
+# build the model
+model = LinearRegression()  
+model.fit(X_train, y_train)
 
+print(model.coef_)
 
+#calculate the predictions of the linear regression model
+y_pred = model.predict(X_test)
+
+#build a new data frame with two columns, the actual values of the test data, 
+#and the predictions of the model
+df_compare = pd.DataFrame({'Actual': y_test, 'Predicted': y_pred})
+df_head = df_compare.head(25)
+print(df_head)
+
+df_head.plot(kind='bar',figsize=(10,8))
+plt.grid(which='major', linestyle='-', linewidth='0.5', color='green')
+plt.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
+plt.show()
+
+print('Mean:', np.mean(y_test))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
+# Regression chart.
+def chart_regression(pred, y, sort=True):
+    t = pd.DataFrame({'pred': pred, 'y': y.flatten()})
+    if sort:
+        t.sort_values(by=['y'], inplace=True)
+    plt.plot(t['y'].tolist(), label='expected')
+    plt.plot(t['pred'].tolist(), label='prediction')
+    plt.ylabel('output')
+    plt.legend()
+    plt.show()
+    
+chart_regression(y_pred[:100].flatten(),y_test[:100],sort=True)   
+
+## RMSE is around 0.4% of the Mean value. This means that it is a relatively good score.
