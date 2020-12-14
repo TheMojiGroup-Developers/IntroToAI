6b896687fd224e2377ef5fadfca67d932dcec06f
Saffan Ahmed <saffan_a@icloud.com>
2020-12-14 15:45:20 +0000

    Creating Matrix Plot.

--- Python Code/LinearRegressionPlot_Years&GlobalSales.py
+++ Python Code/LinearRegressionPlot_Years&GlobalSales.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt  
 from sklearn.model_selection import train_test_split 
 from sklearn.linear_model import LinearRegression
@@ -19,15 +20,23 @@ print(data.head(100)) #Lists the last Top 100 games from the dataset.
 # Plotting Global Sales for Each Platform (Top 100)
 
 # Plotting and Visualising the  Data:
-plt.scatter(data.Name, data.Global_Sales)
-plt.xlabel("Name")
+plt.scatter(data.Year, data.Global_Sales)
+plt.xlabel("Year")
 plt.ylabel("Global_Sales")
 plt.show()
 
 # Plotting data with sub-plot:
-data_plot = data.loc[:,["Name","Global_Sales"]]
+data_plot = data.loc[:,["Year","Global_Sales"]]
 data_plot.plot()
 #filename_read = os.path.join(path, "vgsales.csv")
 #df = pd.read_csv(filename_read, na_values=['NA', '?'])
 #print(df[['Year','Global_Sales']])
 
+# Matrix
+matrix = data.corr()
+plt.figure(figsize=(8,6))
+#plot heat map
+g=sns.heatmap(matrix,annot=True,cmap="YlGn_r")
+# Value closest to 1 shows a strong correlation.
+
+sns.pairplot(data);
