962de6d70a5a20217c61a65e75b8a5fce84132b3
Martin Duong <martinphat@hotmail.co.uk>
2020-12-19 17:32:13 +0000

    Starting on SVM

---
+++ Python Code/SVM.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.svm import SVR
+from sklearn.preprocessing import StandardScaler
+
+
+dataset = pd.read_csv('VideoGameSalesWithRating.csv')
+X = dataset.iloc[:, 10:11].values.astype(float)
+y = dataset.iloc[:, 9:10].values.astype(float)
+
+sc_X = StandardScaler()
+sc_y = StandardScaler()
+X = sc_X.fit_transform(X)
+y = sc_y.fit_transform(y)
+
+regressor = SVR(kernel='')
+regressor.fit(X, y)
