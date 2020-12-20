73252da469cb5b31d28d80e4a3b2308cb3dc2ccb
KevinLa1999 <72019912+KevinLa1999@users.noreply.github.com>
2020-12-20 16:24:37 +0000

    Update LinearRegressionCriticScore&GlobalSales.py
    Split data set to training and testing.

--- Python Code/LinearRegressionCriticScore&GlobalSales.py
+++ Python Code/LinearRegressionCriticScore&GlobalSales.py
@@ -9,11 +9,10 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
 from scipy.stats import pearsonr
-
-#path = "/Users/saffanahmed/Documents/IntroToAI/IntroToAI"
+from sklearn.metrics import r2_score 
 
 data = pd.read_csv(
-    "/Users/saffanahmed/Documents/IntroToAI/IntroToAI/VideoGameSalesWithRating.csv")
+    "/Users/Kevin/Documents/Github/IntroToAI/VideoGameSalesWithRating.csv")
 print(data.info)
 print(data.describe())
 print(data.head(100))  # Lists the first Top 100 games from dataset.
@@ -44,25 +43,32 @@ print('Global Sales x Year_of_Release: %.3f' % corr)
 # Platform isn't constantly changing
 #corr, _ = pearsonr(data.Global_Sales, data.Platform)
 #print('Global Sales x Platform: %.3f' % corr)
-
 corr, _ = pearsonr(data.Global_Sales, data.Genre)
 print('Global Sales x Genre: %.3f' % corr)
 corr, _ = pearsonr(data.Global_Sales, data.Publisher)
 print('Global Sales x Publisher: %.3f' % corr)
 
+#Create barchart for global sales against platforms
 platf_sales_top10 = platf_sales.iloc[0:10, 0:10]
 ax = platf_sales_top10.plot(
     x='Platform', y='Global_Sales', kind='bar', figsize=(23, 5), rot=360)
 ax.get_legend().remove()
-plt.title('Sales by platform', size=14)
+plt.title('Global Sales by platform', size=14)
 plt.xlabel('Platform')
-plt.ylabel('Worldwide sales, M copies')
+plt.ylabel('Global sales in millions')
 plt.show()
 
 # Linear regression plot for Critic Score against Global Sales with best fitted line.
 x = data.Global_Sales.values.reshape(-1, 1)
 y = data.Critic_Score.values.reshape(-1, 1)
+
+#split data into testing and training
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=1/5, random_state=10)
+
 sns.regplot(x="Critic_Score", y="Global_Sales", data=data, ci=None,
             color="blue", line_kws={"color": "red"}, x_jitter=.02).set(ylim=(0, 17.5))
 model = LinearRegression()
-model.fit(x, y)
+model.fit(x,y)
+print(model.coef_)
+
+
