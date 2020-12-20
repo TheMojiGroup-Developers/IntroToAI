33e37b1fa4d0cd1ee843be6ada239bab77cc2c5c
Saffan Ahmed <saffan_a@icloud.com>
2020-12-20 18:59:53 +0000

    Update SVM Model

--- Python Code/SVM.py
+++ Python Code/SVM.py
@@ -4,8 +4,13 @@ import pandas as pd
 from sklearn.svm import SVR
 from sklearn.preprocessing import StandardScaler
 
-#dataset = pd.read_csv('VideoGameSalesWithRating.csv')
-dataset = pd.read_csv('C:/Users/Kevin/Documents/GitHub/IntroToAI/VideoGameSalesWithRating.csv')
+# set your path
+path_kevin = "C:/Users/Kevin/Documents/GitHub/IntroToAI"
+path_saffan = "/Users/saffanahmed/Documents/IntroToAI/IntroToAI/"
+# read in the data as csv
+filename_read = os.path.join(path_saffan, "VideoGameSalesWithRating.csv")
+dataset = pd.read_csv(filename_read)
+
 X = dataset.iloc[:, 10:11].values.astype(float)
 y = dataset.iloc[:, 9:10].values.astype(float)
 
