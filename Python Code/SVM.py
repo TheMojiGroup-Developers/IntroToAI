2e6d451b0af606e583854adf5fc1cce3e60fa3c4
Martin Duong <martinphat@hotmail.co.uk>
2020-12-21 11:35:42 +0000

    Produce RMSE

--- Python Code/SVM.py
+++ Python Code/SVM.py
@@ -1,7 +1,8 @@
 import numpy as np
-import os
 import matplotlib.pyplot as plt
 import pandas as pd
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
 from sklearn.svm import SVR
 from sklearn.preprocessing import StandardScaler
 
@@ -24,10 +25,35 @@ y = sc_y.fit_transform(y)
 regressor = SVR(kernel='rbf')
 regressor.fit(X, y.ravel())
 
+
+# Makes the prediction line more smoother
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color='magenta')
-plt.plot(X, regressor.predict(X), color='green')
+plt.plot(X_grid, regressor.predict(X_grid), color='green')
+##plt.scatter(X, y, color='magenta')
+##plt.plot(X, regressor.predict(X), color='green')
 plt.title('Support Vector Regression Model')
 plt.xlabel('Critic Score')
-plt.ylabel('Global Sales')
+plt.ylabel('Global Sale')
 plt.show()
 
+############################################
+
+x = dataset.Critic_Score.values.reshape(-1, 1)
+y = dataset.Global_Sales.values.reshape(-1, 1)
+
+# trains the data set
+x_train, x_test, y_train, y_test = train_test_split(
+    x, y, test_size=0.20, random_state=42)
+SupportVectorRegModel = SVR()
+SupportVectorRegModel.fit(x_train, y_train)
+
+# creates prediction
+y_pred = SupportVectorRegModel.predict(x_test)
+print(y_pred)
+
+# produce RMSE value
+mse = mean_squared_error(y_test, y_pred)
+rmse = np.sqrt(mse)
+print('Root Mean Squared Error:', rmse)
